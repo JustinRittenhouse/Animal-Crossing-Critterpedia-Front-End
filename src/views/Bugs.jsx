0181cc8d8a429b4e9ef702ce147d770f40627143
Justin Rittenhouse <rittenhouse.justin@gmail.com>
2022-03-19 19:34:51 -0400

    failed select all

--- src/views/Bugs.jsx
+++ src/views/Bugs.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import ReactDOM from 'react-dom'
 
 export const Bugs = () => {
 
@@ -21,25 +22,51 @@ export const Bugs = () => {
   }, [])
 
   const toggleActive = (e) => {
-    if (e.currentTarget.className == "item critInactive") {
-      e.currentTarget.className = "item critActive"
-    } else if (e.currentTarget.className == "item critActive") {
-      e.currentTarget.className = "item critInactive"
-    } else {
+    // if (e.currentTarget.className == "item critInactive") {
+    //   e.currentTarget.className = "item critActive"
+    // } else if (e.currentTarget.className == "item critActive") {
+    //   e.currentTarget.className = "item critInactive"
+    // }
+    e.currentTarget.classList.toggle("critInactive")
+  }
 
+  const selectAll = () => {
+    alert('Boop')
+    const items = document.getElementsByClassName('critInactive')
+    for (let item in items) {
+      item.classList.remove('critInactive')
     }
   }
 
+  const selectNone = () => {
+    
+  }
+
+  const toggleAll = () => {
+
+  }
+
   return (
+    <React.Fragment>
     <div className="itemGrid">
       {bugs.map((bug) => (
-        <div className="item critActive" id={"item" + bug.id} key={"bug" + bug.id} onClick={(e)=>toggleActive(e)}>
+        <div className="item" id={"item" + bug.id} key={"bug" + bug.id} onClick={(e) => toggleActive(e)}>
           <p><b>{bug.name['name-USen']}</b></p>
           <img src={bug.icon_uri} />
-          <p>Months: {bug.availability.isAllYear===true ? "All Year" : bug.availability["month-northern"]}</p>
-          <p>Time: {bug.availability.isAllDay===true ? "All Day" : bug.availability.time}</p>
+          <p>Months: {bug.availability.isAllYear === true ? "All Year" : bug.availability["month-northern"]}</p>
+          <p>Time: {bug.availability.isAllDay === true ? "All Day" : bug.availability.time}</p>
         </div>
       ))}
     </div>
+    <div className='selectorsBox'>
+      <ul className='selectors'>
+        <li className='selector' onClick={() => selectAll()}>Select All</li>
+        <li className='selectorSplitter'>|</li>
+        <li className='selector' onClick={selectNone()}>Select None</li>
+        <li className='selectorSplitter'>|</li>
+        <li className='selector' onClick={toggleAll()}>Toggle All</li>
+      </ul>
+    </div>
+    </React.Fragment>
   )
 }
