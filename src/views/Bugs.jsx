73c1f34d1388ac92fa7737d103b781a94aad33cd
Justin Rittenhouse <rittenhouse.justin@gmail.com>
2022-03-21 12:24:20 -0400

    time travel complete in console

--- src/views/Bugs.jsx
+++ src/views/Bugs.jsx
@@ -22,7 +22,7 @@ export const Bugs = () => {
   const getMonths = () => {
     fetch("src/contexts/months.json")
       .then(res => res.json())
-      .then (data => setMonths(data))
+      .then(data => setMonths(data))
   }
 
   useEffect(() => {
@@ -58,49 +58,71 @@ export const Bugs = () => {
     alert('Bop')
   }
 
+  // I pulled this from user ggorlen on stackoverflow.
   var mode = a => {
     a = a.slice().sort((x, y) => x - y);
-  
+
     var bestStreak = 1;
     var bestElem = a[0];
     var currentStreak = 1;
     var currentElem = a[0];
-  
+
     for (let i = 1; i < a.length; i++) {
-      if (a[i-1] !== a[i]) {
+      if (a[i - 1] !== a[i]) {
         if (currentStreak > bestStreak) {
           bestStreak = currentStreak;
           bestElem = currentElem;
         }
-  
+
         currentStreak = 0;
         currentElem = a[i];
       }
-  
+
       currentStreak++;
     }
-  
+
     return currentStreak > bestStreak ? currentElem : bestElem;
   };
 
   const timeTravel = () => {
-    // console.log(bugs)
-    while (missingBugs.size > 0) {
-      // console.log(Array.from(missingBugs))
+    // This is the main functionality of the website.
+    // First this makes a copy of missingBugs in case the user decides to change missingBugs later.
+    let missingCopy = new Set(missingBugs)
+    // This will eventually be the list of dictionaries of months the user needs to travel to with their respective creatures.
+    let travelMonths = []
+    // This makes an array of all the total months each creature is available.
+    // Then it finds the mode of the months, and returns that.
+    // If any creatures are leftover, it runs again.
+    // console.log(missingCopy.size)
+    while (missingCopy.size > 0) {
       let monthArray = []
+      let travelBugs = []
       for (let bug of bugs) {
-        // console.log(bug.id)
-        if (Array.from(missingBugs).includes(bug.id.toString()))
-        monthArray = [...monthArray,...bug.availability["month-array-northern"]]
-      }
-      let modeMonth = mode(monthArray)
-      console.log(modeMonth)
-      for (let bug of bugs) {
-        if (bug.availability["month-array-northern"].includes(modeMonth)) {
-          missingBugs.delete(bug.id.toString())
+        if (Array.from(missingCopy).includes(bug.id.toString())) {
+          monthArray = [...monthArray, ...bug.availability["month-array-northern"]]
+          travelBugs.push(bug)
         }
       }
-      console.log(missingBugs)
+      let modeMonth = mode(monthArray)
+      // travelMonths[modeMonth] = []
+      let travelMonth = []
+      for (let bug of travelBugs) {
+        if (bug.availability["month-array-northern"].includes(modeMonth)) {
+          travelMonth.push(bug)
+          missingCopy.delete(bug.id.toString())
+        }
+      }
+      let fullTravelMonth = {}
+      fullTravelMonth[modeMonth] = travelMonth
+      travelMonths.push(fullTravelMonth)
+    }
+    console.log(travelMonths)
+    travelMonths.sort(function(x, y) {return Object.values(y).length - Object.values(x).length})
+    for (let bugDict of travelMonths) {
+      console.log(Object.keys(bugDict)[0])
+      for (let bug of Object.values(bugDict)[0]) {
+        console.log(bug.name['name-USen'], bug.availability.time, bug.availability.location)
+      }
     }
   }
 
@@ -117,18 +139,18 @@ export const Bugs = () => {
         ))}
       </div>
       {/* <div className='boxBox'> */}
-        <div className='selectorsBox'>
-          <ul className='selectors'>
-            <li className='selector' onClick={() => selectAll()}>Select All</li>
-            <li className='selectorSplitter'>|</li>
-            <li className='selector' onClick={() => selectNone()}>Select None</li>
-            <li className='selectorSplitter'>|</li>
-            <li className='selector' onClick={() => toggleAll()}>Toggle All</li>
-          </ul>
-        </div>
-        <div className='resultButtonBox' onClick={() => timeTravel()}>
-          <h4 >Time Travel</h4>
-        </div>
+      <div className='selectorsBox'>
+        <ul className='selectors'>
+          <li className='selector' onClick={() => selectAll()}>Select All</li>
+          <li className='selectorSplitter'>|</li>
+          <li className='selector' onClick={() => selectNone()}>Select None</li>
+          <li className='selectorSplitter'>|</li>
+          <li className='selector' onClick={() => toggleAll()}>Toggle All</li>
+        </ul>
+      </div>
+      <div className='resultButtonBox' onClick={() => timeTravel()}>
+        <h4 >Time Travel</h4>
+      </div>
       {/* </div> */}
     </React.Fragment>
   )
