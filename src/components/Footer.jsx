065bea9cb4ab336f955dc4c59cdecc886bbb3602
Justin Rittenhouse <rittenhouse.justin@gmail.com>
2022-03-23 19:36:26 -0400

    footer written

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Footer = () => {
 
     return (
         <React.Fragment>
-            <div>Footer</div>
+            <div className='credits'>
+            <div className='creditColumn'>
+                <div className='creditGroup'>
+                    <p>- Homepage art by &nbsp;</p>
+                    <Link to="https://www.instagram.com/meliheyo_art/">meliheyo_art</Link>
+                </div>
+                <div className='creditGroup'>
+                    <p>- Background art by &nbsp;</p>
+                    <Link to="https://www.deviantart.com/biochao">Biochao</Link>
+                </div>
+            </div>
+            <div className='creditColumn'>
+                <div className='creditGroup'>
+                    <p>- Fink Heavy font by &nbsp;</p>
+                    <Link to="https://www.fontsmarket.com/fonts/yifei">yifei</Link>
+                </div>
+                <div className='creditGroup'>
+                    <p>- Animal Crossing and its images are all properties of Nintendo Co., Ltd.</p>
+                </div>
+            </div>
+            </div>
+            <div className='selfGlorification'>
+                <p>The Time Traveler's Critterpedia was created by Justin Rittenhouse</p>
+            </div>
         </React.Fragment>
     )
 }
